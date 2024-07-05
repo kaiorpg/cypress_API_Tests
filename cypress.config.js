5c49ebe4b6c2514edade070d87936a9bc99450eb
Kaio Rafael Perdigão de Castro <kaiorpg@gmail.com>
2024-07-04 23:56:23 -0300

    Adicionando pasta para screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
   e2e: {
     baseUrl: 'https://api.restful-api.dev',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  
 });
